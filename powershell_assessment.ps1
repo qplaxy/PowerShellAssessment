a8b5e3570408bcacfeb0fc6ecce694ad084ba9f0
Valeria <39573018+valeria-ortiz7@users.noreply.github.com>
2021-05-27 21:57:40 -0600

    [UPDATE] PowerShell Assessment
    1. Added important note in the comments  
    2. Modified the comparison to a case-insensitive comparison as paths do not distinguish this.

--- powershell_assessment.ps1
+++ powershell_assessment.ps1
@@ -1,9 +1,16 @@
 ﻿<# PowerShell assessment for GHD Automation team
 Valeria Ortiz León #>
 
-<# Important note: 
-For assessment purposes, I added a prompt to press Enter to exit and did not make it an autoexecutable script 
-because of some "Write-Host" messages that were added for visibility. #>
+<# Important notes: 
+1. For assessment purposes, I added a prompt to press Enter to exit and did not make it an autoexecutable script 
+because of some "Write-Host" messages that were added for visibility. 
+
+2. In the project description, I noticed something in the given directory: 
+%appdata%\Microsoft\teams\databases. 
+
+This directory contains a "." at the end in the specification. 
+This "." was added to the name in the "filesToRemove" array to follow the instructions but must de deleted to clear the files inside the real %appdata%\Microsoft\teams\databases folder.
+#>
 
 <# Script #>
 
@@ -17,7 +24,7 @@ $directory = $env:AppData + "\Microsoft\Teams"
 $filesToRemove = @("application cache/cache",
                    "blob_storage",
                    "Cache",
-                   "databases",
+                   "databases.",
                    "GPUCache",
                    "IndexedDB",
                    "Local Storage",                   
@@ -47,7 +54,7 @@ $archivos = Get-ChildItem -Name
 foreach ($folder in $filesToRemove)
 {
     # Checks if inside  C:/Users/%current user%/AppData/Roaming/Microsoft/Teams those folders exist using the array previously created with Get-ChildItem
-    if ($archivos -ceq $folder)
+    if ($archivos -eq $folder)
         {
            # Sets the current directory temporarily to the folder we are cleaning
            $current_directory = $directory + "/" + $folder
@@ -63,3 +70,4 @@ foreach ($folder in $filesToRemove)
 # Writes a message to the user letting them know that the cache files have been deleted. Prompts to Exit so the messages written can be read.
 Write-Host "Cache files have been deleted."
 Read-Host -Prompt "Press Enter to exit"
+exit
